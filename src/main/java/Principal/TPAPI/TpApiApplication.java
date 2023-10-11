275158736ea9ef9495bbe0b4fd7fad348d7f796e
AlexisCepeda <alexisnatel@gmail.com>
2023-10-11 18:27:07 -0300

    Pagination added

---
+++ src/main/java/Principal/TPAPI/TpApiApplication.java
@@ -0,0 +1,13 @@
+package Principal.TPAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TpApiApplication.class, args);
+		System.out.println("Estoy funcionando ;)");
+	}
+}
---
+++ src/main/java/Principal/TPAPI/repositories/PersonaRepository.java
@@ -0,0 +1,57 @@
+package Principal.TPAPI.repositories;
+
+import Principal.TPAPI.entities.Persona;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface PersonaRepository extends BaseRepository<Persona, Long> {
+
+    //Anotacion Metodo de Query
+    List<Persona> findByNombreContainingOrApellidoContaining(String nombre, String apellido);
+
+    boolean existsByDni(int dni);
+
+    //Anotacion JPQL parametros indexados
+    @Query(value = "SELECT p FROM Persona p WHERE p.nombre LIKE %?1% OR p.apellido LIKE %?1%")
+    List<Persona> search(String filtro);
+
+    //Anotacion JPQL parametros nombrados
+    @Query(value = "SELECT p FROM Persona p WHERE p.nombre LIKE %:filtro% OR p.apellido LIKE %:filtro%")
+    List<Persona> search2(@Param("filtro") String filtro);
+
+    //Anotacion JPQL conn Query Nativo
+    @Query(
+            value = "SELECT * FROM persona WHERE persona.nombre LIKE %:filtro% OR persona.apellido LIKE %:filtro%",
+            nativeQuery = true
+    )
+    List<Persona> searchNativo(String filtro);
+
+
+    //CON PAGINACION
+    //Anotacion Metodo de Query
+    Page<Persona> findByNombreContainingOrApellidoContaining(String nombre, String apellido, Pageable pageable);
+
+    //Anotacion JPQL parametros indexados
+    @Query(value = "SELECT p FROM Persona p WHERE p.nombre LIKE %?1% OR p.apellido LIKE %?1%")
+    Page<Persona> search(String filtro, Pageable pageable);
+
+    //Anotacion JPQL parametros nombrados
+    @Query(value = "SELECT p FROM Persona p WHERE p.nombre LIKE %:filtro% OR p.apellido LIKE %:filtro%")
+    Page<Persona> search2(@Param("filtro") String filtro, Pageable pageable);
+
+    //Anotacion JPQL conn Query Nativo
+    @Query(
+            value = "SELECT * FROM persona WHERE persona.nombre LIKE %:filtro% OR persona.apellido LIKE %:filtro%",
+            countQuery = "SELECT count(*) FROM persona",
+            nativeQuery = true
+    )
+    //Page<Persona> searchNativo(String filtro);
+    Page<Persona> searchNativo(String filtro, Pageable pageable);
+
+}
